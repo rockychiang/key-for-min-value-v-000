bb11b6155c97683d3abda4c1da5dcc924524be86
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-11 04:41:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 def key_for_min_value(name_hash)
   key = {none: nil}
-  name_hash.each {|item, no| key[low: item] if no < key[:none]}
+  name_hash.each do |item, no| 
+    key[low: item] if no < key[:none] || key[:none] == nil
+  end
   key[:low]
 end
